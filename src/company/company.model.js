3bfb9f13617adecafd0ff9a412a8a653e7cd8396
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-24 20:28:14 -0600

    The creat to de document this company, and create to de model the company

---
+++ src/company/company.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const CompanySchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        require: [true, 'Company name is required']
+    },
+    telefono: {
+        type: String,
+        require: [true, 'Company phone number is required']
+    },
+    nivelInpacto:{ 
+        type: String,
+        require: [true, 'The companys impact level is mandatory']
+    },
+    añosTrayectoria: {
+        type: Number,
+        require: [true, 'The years of experience of the company is mandatory']
+    },
+    categoria:{
+        type: String,
+        require: [true, 'The category of the company is mandatory']
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    }
+});
+
+CompanySchema.methods.toJSON = function(){
+    const { __v, password, _id, ...company} = this.toObject();
+    company.uid = _id;
+     return company;
+}
+
+
+export default mongoose.model('Company', CompanySchema);
